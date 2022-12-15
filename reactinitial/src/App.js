6ed92872b29f3930d49a748d9a3066823455db0f
adamroka <109951253+adamroka@users.noreply.github.com>
2022-12-15 12:00:19 +0100

    Update App.js

--- reactinitial/src/App.js
+++ reactinitial/src/App.js
@@ -6,7 +6,40 @@ const App = () => {
     <div>
       <button type="button" onclick="myFunction(id)"> Show more </button>
     </div>
+    
     <script>
+      async function getTeams() {
+            let url = 'https://demoapi.com/api/teams';
+            try {
+                let res = await fetch(url);
+                return await res.json();
+            } catch (error) {
+                console.log(error);
+            }
+      }
+  
+       async function renderTeams() {
+            let users = await getTeams();
+            let html = '';
+            teams.forEach(team => {
+                let htmlSegment = `<div class="team">
+                                    <button type="button" onclick="myFunction(this.id)"> Show more </button>
+                                    <div class="info">
+                                        <br> Name: ${team.name}
+                                        <br> Stadium: ${team.stadium}
+                                    </div>
+                                </div>`;
+
+                html += htmlSegment;
+            });
+
+            let container = document.querySelector('.container');
+            container.innerHTML = html;
+        }
+
+        renderTeams();
+  
+  
       function myFunction(clicked_id) {
             var moreText = document.getElementById("more"+clicked_id);
             var btnText = document.getElementById(clicked_id);
